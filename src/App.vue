20a01f2ab613d467425770601c028ee56b165c53
patrickstar <971602307@qq.com>
2024-07-04 17:29:26 +0800

    增加vuex状态管理

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        <!-- store中的counter -->
+        <h2>App当前计数:{{ $store.state.counter }}</h2>
+        <hr>
+        <!-- home页面 -->
+        <home></home>
+    </div>
+</template>
+
+<script setup>
+
+    import Home from './views/Home.vue'
+
+</script>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="home">
+        <h2>Home当前计数: {{ $store.state.counter }}</h2>
+        <h2>Computed当前计数: {{ storeCounter }}</h2>
+        <h2>Setup当前计数: {{ counter }}</h2>
+        <button @click="increment">+1</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            storeCounter() {
+                return this.$store.state.counter
+            }
+        }
+    }
+</script>
+
+<script setup>
+import { toRefs } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+//这里拿到的数据不是响应式的,解构也不行 ，可以通过toRefs转换
+const { counter } = toRefs(store.state)
+
+function increment() {
+    // console.log(store.state.counter);
+    // store.state.counter++
+    store.commit("increment")
+}
+</script>
+
+<style scoped></style>
