4fd5520a251b71cf61e777991e95eda56543fcb8
patrickstar <971602307@qq.com>
2024-07-05 01:20:41 +0800

    更新文件名

---
+++ src/hooks/useState.js
@@ -0,0 +1,14 @@
+import { computed } from 'vue'
+import { useStore, mapState } from 'vuex'
+
+export default function useState(mapper) {
+    const store = useStore()
+    const stateFnObj = mapState(mapper)
+    
+    const newState = {}
+    Object.keys(stateFnObj).forEach(key => {
+        newState[key] = computed(stateFnObj[key].bind({ $store: store }))
+    })
+
+    return newState
+}
---
+++ src/views/02_核心一State-mapState映射.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="home">
+        <button @click="incrementLevel">修改Level</button>
+        <!-- 1 在模板中直接使用多个状态 -->
+        <h2>name: {{ $store.state.name }}</h2>
+        <h2>level: {{ $store.state.level }}</h2>
+        <h2>avatar: {{ $store.state.avatarURL }}</h2>
+
+        <!-- 2. 计算属性（映射状态：数组语法） -->
+        <!-- <h2>name: {{ name }}</h2>
+        <h2>level: {{ level }}</h2> -->
+
+        <!-- 3. 计算属性（映射状态：对象语法） -->
+        <!-- <h2>name: {{ sName }}</h2>
+        <h2>level: {{ sLevel }}</h2> -->
+
+        <!-- 4. setup计算属性（映射状态：对象语法） -->
+        <!-- <h2>name: {{ cName }}</h2>
+        <h2>level: {{ cLevel }}</h2> -->
+
+        <!-- 5. setup计算属性（映射状态：对象语法） -->
+        <h2>name: {{ name }}</h2>
+        <h2>level: {{ level }}</h2>
+    </div>
+</template>
+
+<script>
+// import { mapState } from "vuex"
+// export default {
+//     computed: {
+//         fullname() {
+//             return "xxx"
+//         },
+//         ...mapState(["name", "level", "avatarURL"]),
+//         ...mapState({
+//             sName: state => state.name,
+//             sLevel: state => state.level
+//         })
+//     }
+// }
+</script>
+
+<script setup>
+    import { computed, toRefs } from 'vue'
+    import { mapState, useStore } from 'vuex'
+    import useState from '../hooks/useState.js'
+    
+    //1. 一步步完成
+    // const { name, level } = mapState(["name", "level"])
+    // const store = useStore()
+    // const cName = computed(name.bind({ $store: store }))
+    // const cLevel = computed(level.bind({ $store: store }))
+
+    //2. 使用useState
+    // const { name, level } = useState(["name", "level"])
+
+    //3. 直接对store.state进行解构
+    const store = useStore()
+    const { name, level } = toRefs(store.state)
+
+    function incrementLevel() {
+        store.state.level++
+    }
+    
+</script>
+
+<style scoped></style>
